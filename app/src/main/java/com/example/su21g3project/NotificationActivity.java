8a73ad55ee28fc087ec4068b3531d8bd26a73cd5
Vinhpham2412 <vinhpxhe130801@fpt.edu.vn>
2021-06-29 22:47:29 +0700

    Create NotificationActivity

---
+++ app/src/main/java/com/example/su21g3project/NotificationActivity.java
@@ -0,0 +1,57 @@
+package com.example.su21g3project;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import adapter.NotificationAdapter;
+import model.Notification;
+
+public class NotificationActivity extends AppCompatActivity {
+    private List<Notification> notificationList;
+    private RecyclerView recyclerView;
+    private DatabaseReference reference;
+    private NotificationAdapter notificationAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+        recyclerView=findViewById(R.id.recycleViewNotice);
+        notificationList=new ArrayList<>();
+        reference= FirebaseDatabase.getInstance().getReference("Notifications");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                notificationList.clear();
+                for (DataSnapshot dataSnapshot :snapshot.getChildren()){
+                    Notification notification=dataSnapshot.getValue(Notification.class);
+                    if(notification.getReceiverId().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())){
+                        notificationList.add(notification);
+                    }
+                    notificationAdapter=new NotificationAdapter(NotificationActivity.this,notificationList);
+                    recyclerView.setAdapter(notificationAdapter);
+                    recyclerView.setLayoutManager(new LinearLayoutManager(NotificationActivity.this));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/model/Notification.java
@@ -0,0 +1,62 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Notification {
+    private String senderId;
+    private String receiverId;
+    private String message;
+    private LocalDateTime time;
+    private int type;
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(String receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public Notification(String senderId, String receiverId, String message, LocalDateTime time, int type) {
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.message = message;
+        this.time = time;
+        this.type = type;
+    }
+
+    public Notification() {
+    }
+}
